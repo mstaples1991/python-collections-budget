968f426395de656ea697c3f4309d113a04f03484
Matt Staples <mstaples1991@gmail.com>
2020-12-20 14:55:00 -0500

    Finish making BudgetList an iterable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -23,13 +23,13 @@ class BudgetList:
         return len(self.expenses) + len(self.overages)
 
     def __iter__(self):
-        self.iter_exp = iter(self.expenses)
+        self.iter_e = iter(self.expenses)
         self.iter_o = iter(self.overages)
         return self
 
     def __next__(self):
         try:
-            return next(self.iter_exp)
+            return next(self.iter_e)
         except StopIteration as stop:
             return next(self.iter_o)
 
